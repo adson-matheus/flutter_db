ba033002d0f44628c5e0519babd1e856c0a51dcb
adson62 <adson.matheus62@gmail.com>
2022-02-23 16:36:55 -0300

    add metodos a classe Human, inserindo um objeto Human

--- lib/human.dart
+++ lib/human.dart
@@ -9,16 +9,39 @@ class Human {
 
   Human({required this.id, required this.name, required this.age});
 
-  void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-
-    final database = openDatabase(
-      join(await getDatabasesPath(), 'human.db'),
-      onCreate: (db, version) {
-        return db.execute(
-            'CREATE TABLE Human (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age TEXT NOT NULL)');
-      },
-      version: 1,
-    );
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+    };
   }
+
+  @override
+  String toString() {
+    return 'Human{id: $id, name: $name, age: $age}';
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = openDatabase(
+    join(await getDatabasesPath(), 'human.db'),
+    onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE Human (id INTEGER PRIMARY KEY, name TEXT NOT NULL, age TEXT NOT NULL)');
+    },
+    version: 1,
+  );
+
+  Future<void> insertHuman(Human human) async {
+    final db = await database;
+    await db.insert('Human', human.toMap(),
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  var adson = Human(id: 0, name: 'Adson', age: 21);
+
+  await insertHuman(adson);
 }
